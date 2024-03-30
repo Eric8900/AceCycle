d62de6f8f32695ec3cf781d6a009e46c243fdd84
Eric8900 <ericchen890@gmail.com>
2024-03-30 18:45:08 -0500

    test

--- src/components/AboutTeam.tsx
+++ src/components/AboutTeam.tsx
@@ -5,9 +5,9 @@ function AboutTeam() {
 
     return (
       <>
-        <div className="flex flex-col justify-center items-center h-full w-full text-center my-10">
-            <h1 className="text-gray-800 md:text-7xl text-6xl font-extrabold">Our Team</h1>
-            <p className="opacity-80 text-gray-700 md:text-lg">Jordan High School | Seven Lakes High School</p>
+        <div className="flex flex-col justify-center items-center h-full w-full my-10">
+            <h1 className="text-gray-800 md:text-7xl text-6xl font-extrabold text-center">Our Team</h1>
+            <p className="opacity-80 text-gray-700 md:text-lg text-center">Jordan High School | Seven Lakes High School</p>
             <div className="flex flex-col md:flex-row flex-wrap gap-10 items-center justify-center m-10">
                 {team.map((item, index) => (
                   <a href={item.linkedin}
