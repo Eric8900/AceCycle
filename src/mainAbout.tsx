eb2f65be1c321d43f63fddcc255b0f7437bfe242
Eric8900 <ericchen890@gmail.com>
2024-04-22 19:18:30 -0500

    add analytics

--- src/mainAbout.tsx
+++ src/mainAbout.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AboutApp from './AboutApp'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/>
     <AboutApp/>
   </React.StrictMode>,
 )
--- src/mainContact.tsx
+++ src/mainContact.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import ContactApp from './ContactApp'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/>
     <ContactApp/>
   </React.StrictMode>,
 )
