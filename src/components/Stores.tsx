deeb1c0b63342902db6b5bc9679dcec91f292eb4
Eric8900 <89477025+Eric8900@users.noreply.github.com>
2025-01-07 17:02:11 -0600

    Update Stores.tsx

--- src/components/Stores.tsx
+++ src/components/Stores.tsx
@@ -6,7 +6,7 @@ function Stores() {
                     <b className="bg-gradient-to-r from-lime-400 to-lime-500 bg-clip-text text-transparent">AceCycle</b> for Stores
                 </h1>
                 <p className="text-lg mb-8">
-                    Manage tennis ball inventory and track usage with <b className="bg-gradient-to-r from-lime-400 to-lime-500 bg-clip-text text-transparent">AceCycleManager</b>, coming soon.
+                    Manage tennis ball inventory and track usage with <b className="bg-gradient-to-r from-lime-400 to-lime-500 bg-clip-text text-transparent">AceCycleManager</b>.
                 </p>
                 <a target="_blank" rel="noopener noreferrer" href={'https://github.com/Nishant27-2006/AceCycleManager'} className="">
                     <button className="bg-primary hover:bg-green-500 text-white font-bold py-3 px-12 rounded-[10px] transition duration-300">
