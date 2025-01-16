3822c4aae704ced479360f13a9c9390374cb3c9e
Eric8900 <ericchen890@gmail.com>
2025-01-16 16:38:35 -0600

    gallery add

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -114,7 +114,7 @@ const content = [
 ];
 
 const numbersToSkip = [8, 9];
-const data = Array.from({ length: 20 })
+const data = Array.from({ length: 21 })
   .map((_, i) => i + 1)
   .filter(num => !numbersToSkip.includes(num))
   .map(num => ({
